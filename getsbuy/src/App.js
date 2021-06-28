b72ee41addc5367f6d4ffe9a408257b7f96a4a49
Milan Rawat <milanrawat086@gmail.com>
2021-06-28 20:53:11 +0530

    Signin page created Signin page created using material-ui

--- getsbuy/src/App.js
+++ getsbuy/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Home from "./components/Pages/Home";
 import Signup from "./components/Pages/Signup";
+import Signin from "./components/Pages/Signin";
 
 import { Redirect, Route } from "react-router-dom";
 
@@ -12,6 +13,7 @@ function App() {
       </Route>
       <Route path="/" exact={true} component={Home} />
       <Route path="/signup" component={Signup} />
+      <Route path="/signin" component={Signin} />
     </div>
   );
 }
