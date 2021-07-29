f83479a0a8bb599c5f9602d7e56b264a40b72ed4
Milan Rawat <milanrawat086@gmail.com>
2021-07-29 16:36:54 +0530

    get all users route and control added

---
+++ controllers/userController.js
@@ -0,0 +1,17 @@
+const User = require('../models/userModel');
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const allUsers = await User.find();
+
+    res.status(200).json({
+      status: 'success',
+      users: allUsers,
+    });
+  } catch (err) {
+    res.status(503).json({
+      status: 'fail',
+      message: 'Something went wrong! Please try again later',
+    });
+  }
+};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const userAuthController = require('../controllers/userAuthController');
+const userController = require('../controllers/userController');
 
 const router = express.Router();
+
+router.route('/').get(userController.getAllUsers);
 
 router.post('/signup', userAuthController.userSignup);
 
