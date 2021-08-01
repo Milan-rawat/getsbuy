b5c1fe020f6e59c8f5b2c191ae1b69609549f691
Milan Rawat <milanrawat086@gmail.com>
2021-08-01 08:57:07 +0530

    Error Control middleware added

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const User = require('./models/userModel');
 const userRouter = require('./routes/userRoute');
 
 // Start express app
@@ -13,9 +12,23 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
