5f28b6b5ea3c66791097ddfb670c625743ee906b
Milan Rawat <milanrawat086@gmail.com>
2021-08-02 09:05:48 +0530

    onClick error on ui component fixed

--- getsbuy/src/components/Layout/Header/Header.js
+++ getsbuy/src/components/Layout/Header/Header.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import classes from "./Header.module.css";
-import { Backdrop, SideDrawer } from "../Drawer/SideDrawer";
+import React, { useState } from 'react';
+import classes from './Header.module.css';
+import { Backdrop, SideDrawer } from '../Drawer/SideDrawer';
 
-import MenuIcon from "@material-ui/icons/Menu";
-import IconButton from "@material-ui/core/IconButton";
-import AccountCircle from "@material-ui/icons/AccountCircle";
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
-import SearchIcon from "@material-ui/icons/Search";
-import { Link } from "react-router-dom";
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import SearchIcon from '@material-ui/icons/Search';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -28,11 +28,8 @@ const Header = () => {
         <div className={classes.leftTopNav}>
           {menuOpen && <Backdrop onCloseMenu={closeDrawerMenu} />}
           <SideDrawer openMenu={menuOpen} />
-          <IconButton>
-            <MenuIcon
-              className={classes.topNavButton}
-              onClick={toggleDrawerMenu}
-            />
+          <IconButton onClick={toggleDrawerMenu}>
+            <MenuIcon className={classes.topNavButton} />
           </IconButton>
           <div className={classes.applicationTitle}>Getsbuy</div>
         </div>
